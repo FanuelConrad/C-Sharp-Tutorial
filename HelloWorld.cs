5cb8b1d794e33a71d7f91fce072454628e88d804
FanuelConrad <59276995+FanuelConrad@users.noreply.github.com>
2022-04-26 11:33:11 +0300

    updated

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program{
+        static void Main(string[] args){
+            // Console.WriteLine("HelloWorld!");
+            // Console.WriteLine("What is your name?");
+            // string str=Console.ReadLine();
+            // Console.WriteLine("Why, hello there " + str);
+            for(int i=0; i <10; i++){
+Console.WriteLine("The value of i is {0}", i);}
+        }
+    }
+}
